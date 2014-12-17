8f69b276575c75f215ba2116ea650c558cf3fee6
Paul Fioravanti <paul.fioravanti@gmail.com>
2014-12-17 17:41:53 +1100

    Refactor code to get rid of variable

--- fizzbuzz.rs
+++ fizzbuzz.rs
@@ -12,18 +12,12 @@ fn div_by_fifteen(num: int) -> bool {
 
 fn main() {
   for num in range(1i, 101) {
-    let answer =
-      if div_by_fifteen(num) {
-        "FizzBuzz".to_string()
-      } else if div_by_three(num) {
-        "Fizz".to_string()
-      } else if div_by_five(num) {
-        "Buzz".to_string()
-      } else {
-        num.to_string()
-      };
-
-    println!("{}", answer);
+    println!("{:s}",
+      if div_by_fifteen(num) { "FizzBuzz".to_string() }
+      else if div_by_three(num) { "Fizz".to_string() }
+      else if div_by_five(num) { "Buzz".to_string() }
+      else { num.to_string() }
+    );
   }
 }
 
